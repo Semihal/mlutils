fa0d4826e99bd0ce9a917e1e63eabefa87b12b85
Сергей Михайлин <si.mikhaylin@jet.su>
2019-10-17 16:06:22 +0300

    Initial commit

---
+++ mlutils/tensorflow.py
@@ -0,0 +1,35 @@
+import os
+from typing import Union, List
+
+
+def set_used_gpu(gpu_ids: Union[List[int], int]):
+    from tensorflow.python.client import device_lib
+    all_gpu_available = [
+        int(device.name.split(':')[-1])
+        for device in device_lib.list_local_devices()
+        if device.device_type == 'GPU'
+    ]
+    if len(all_gpu_available) == 0:
+        raise GPUNotAvailable('GPU is not available.')
+
+    gpu_ids = [gpu_ids] if isinstance(gpu_ids, int) else gpu_ids
+    available_gpu = []
+    for gpu_id in gpu_ids:
+        if gpu_id in available_gpu:
+            available_gpu.append(gpu_id)
+        else:
+            print(f'Warning: GPU {gpu_id} is not available.')
+    if len(available_gpu) == 0:
+        raise GPUNotAvailable(f'GPU is {available_gpu} is not available. Maybe used f{all_gpu_available}?')
+
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'  # see issue #152
+    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(available_gpu)
+
+
+def set_used_cpu():
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'  # see issue #152
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+
+class GPUNotAvailable(Exception):
+    pass
---
+++ mlutils/video.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import cv2
+
+
+def get_specific_frame(path_to_video: Path, frame_id: int):
+    if not path_to_video.exists():
+        raise FileExistsError(f'Video {path_to_video} not exists.')
+    video = cv2.VideoCapture(str(path_to_video))
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+    is_success, frame = video.read()
+    if is_success:
+        return frame
+    else:
+        raise FrameNotFound(f'Frame {frame_id} not exists in video {path_to_video}')
+
+
+class FrameNotFound(Exception):
+    pass
