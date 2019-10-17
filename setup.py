da1cb17f3304fd3b21a8adfb9ff67cc0609869a5
si.mikhaylin@jet.su <dbsn32hnlK>
2019-10-17 16:36:26 +0300

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mlutils',
+    version='0.0.1',
+    packages=['mlutils'],
+    url='https://github.com/Semihal/mlutils',
+    author='Sergey Mikhaylin',
+    author_email='semihal.devs@gmail.com',
+    python_requires='>=3.6.5',
+)
