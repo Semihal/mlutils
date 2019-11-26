7f5c292f4942bf7747c6736771c76ad97b506959
Сергей Михайлин <si.mikhaylin@jet.su>
2019-11-26 17:49:39 +0300

    Added method data::sparse_from_series

--- mlutils/data.py
+++ mlutils/data.py
@@ -1,5 +1,9 @@
 from typing import Iterable, List
 
+from scipy.sparse import csr_matrix
+import pandas as pd
+import numpy as np
+
 
 def chunk_to_batch(x: Iterable, batch_size: int):
     """
@@ -29,3 +33,34 @@ def chunk_to_batch(x: Iterable, batch_size: int):
         x[i:i+batch_size]
         for i in range(0, len(x), batch_size)
     ]
+
+
+def sparse_from_series(series: pd.Series) -> csr_matrix:
+    """
+    Encodes pandas series of lists int sparse format.
+    Parameters
+    ----------
+    series : pd.Series
+        Series of int lists.
+
+    Returns
+    -------
+    scipy.sparse.csr_matrix:
+        Sparse format.
+
+    Examples
+    -------
+    >>> from mlutils.data import sparse_from_series
+    >>> import pandas as pd
+
+    >>> transactions = pd.Series([[0, 1, 2], [1, 2, 3], [0, 1]])
+    >>> r = sparse_from_series(transactions)
+    >>> r.toarray()
+    array([[1, 1, 1, 0],
+           [0, 1, 1, 1],
+           [1, 1, 0, 0]])
+    """
+    indices = np.hstack(series.values)
+    data = [1] * indices.size
+    indptr = [0] + np.cumsum(series.apply(len)).tolist()
+    return csr_matrix((data, indices, indptr))
