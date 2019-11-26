efc47792c92b2a200706650c92ada02d1b73a229
Сергей Михайлин <si.mikhaylin@jet.su>
2019-11-26 17:30:25 +0300

    Add docstring for data and video modules

--- mlutils/data.py
+++ mlutils/data.py
@@ -1,6 +1,21 @@
+from typing import Iterable, List
 
 
-def chunk_to_batch(x, batch_size):
+def chunk_to_batch(x: Iterable, batch_size: int):
+    """
+    Split list into batches.
+    Parameters
+    ----------
+    x : Iterable
+        List to split.
+    batch_size : int
+        Number of batches.
+
+    Returns
+    -------
+    List
+        batches list of x.
+    """
     return [
         x[i:i+batch_size]
         for i in range(0, len(x), batch_size)
--- mlutils/video.py
+++ mlutils/video.py
@@ -1,8 +1,34 @@
 from pathlib import Path
+from typing import Union
+
 import cv2
 
 
-def get_specific_frame(path_to_video: Path, frame_id: int):
+def get_specific_frame(path_to_video: Union[str, Path], frame_id: int):
+    """
+    Get specify frame from video.
+    Parameters
+    ----------
+    path_to_video: str or Path
+        Path to video file.
+    frame_id: int
+        Number of the desired frame in the video.
+
+    Returns
+    -------
+    np.array
+        Desired video frame.
+
+
+    Raises
+    -------
+    FileExistsError
+        if path_to_video file not exists.
+    FrameNotFound
+        if frame_id not exists in video (frame_id more then number of all frames or less then 0).
+
+    """
+    path_to_video = Path(path_to_video)
     if not path_to_video.exists():
         raise FileExistsError(f'Video {path_to_video} not exists.')
     video = cv2.VideoCapture(str(path_to_video))
