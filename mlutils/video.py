8d1a6d54d380624e250bcc6cb6a99b948001733a
Сергей Михайлин <si.mikhaylin@jet.su>
2020-03-04 18:46:56 +0300

    video: added ImageReader and VideoReader and rewrite get_specific_frame

--- mlutils/video.py
+++ mlutils/video.py
@@ -1,10 +1,78 @@
 from pathlib import Path
-from typing import Union
 
 import cv2
 
+from mlutils.path import PathType, PathsType, check_path_type
 
-def get_specific_frame(path_to_video: Union[str, Path], frame_id: int):
+
+class FrameNotFound(Exception):
+    pass
+
+
+class ImagesReader:
+
+    def __init__(self, file_names: PathsType, read_flag=cv2.IMREAD_COLOR):
+        """
+        Class for iterate by images.
+        Parameters
+        ----------
+        file_names:
+            Path(s) to images for future iterate.
+        read_flag:
+            Flag for cv2.imread function.
+        """
+        self.paths = check_path_type(file_names, check_exists=True, as_list=True)
+        self.read_flag = read_flag
+        self._current_id = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._check_current_id()
+        current_path = self.paths[self._current_id]
+        image = cv2.imread(current_path, self.read_flag)
+        self._current_id += 1
+        return image
+
+    def _check_current_id(self):
+        if self._current_id == len(self.paths):
+            raise StopIteration
+
+
+class VideoReader:
+
+    def __init__(self, file_names: PathsType, config=None):
+        """
+        Class for iterate by video frames.
+        Parameters
+        ----------
+        file_names:
+            Path to video file for future frame iterations.
+        config:
+            Config for cv2.set function.
+        """
+        self.path = check_path_type(file_names, check_exists=True, as_list=False)
+        self.video = cv2.VideoCapture(self.path)
+        if not self.video.isOpened():
+            raise IOError('Video {} cannot be opened'.format(self.path))
+
+        self.config = config
+        if config is not None:
+            for key, value in config.items():
+                self.video.set(key, value)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        was_read, img = self.video.read()
+        if not was_read:
+            raise StopIteration
+        return img
+
+
+def get_specific_frame(path_to_video: PathType, frame_id: int):
     """
     Get specify frame from video.
     Parameters
@@ -19,26 +87,18 @@ def get_specific_frame(path_to_video: Union[str, Path], frame_id: int):
     np.array
         Desired video frame.
 
-
     Raises
     -------
     FileExistsError
         if path_to_video file not exists.
     FrameNotFound
         if frame_id not exists in video (frame_id more then number of all frames or less then 0).
-
     """
-    path_to_video = Path(path_to_video)
-    if not path_to_video.exists():
-        raise FileExistsError(f'Video {path_to_video} not exists.')
-    video = cv2.VideoCapture(str(path_to_video))
-    video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
-    is_success, frame = video.read()
-    if is_success:
-        return frame
-    else:
+    config_for_reader = {
+        cv2.CAP_PROP_POS_FRAMES: frame_id
+    }
+    reader = VideoReader(path_to_video, config_for_reader)
+    try:
+        return next(reader)
+    except StopIteration:
         raise FrameNotFound(f'Frame {frame_id} not exists in video {path_to_video}')
-
-
-class FrameNotFound(Exception):
-    pass
